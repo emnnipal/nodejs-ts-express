ea768ca431d457ad91ca4bf370ec0c6eb30413bb
Emmanuel Nipal <emnnipal@gmail.com>
2022-04-14 21:56:47 +0800

    fix: upgrade pnpm action version and add cluster start

--- src/shared/configs/app.ts
+++ src/shared/configs/app.ts
@@ -2,7 +2,4 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const NODE_ENV = process.env.NODE_ENV || 'development';
-export const APP = process.env.APP || '';
-export const PORT = process.env.PORT || '';
-export const LOGGER_LEVEL = process.env.LOGGER_LEVEL || 'info';
+export const { NODE_ENV = 'development', APP = '', PORT = '', LOGGER_LEVEL = 'info' } = process.env;
